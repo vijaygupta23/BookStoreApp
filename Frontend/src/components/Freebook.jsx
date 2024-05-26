651329a0bf4acd54ab16ec74c21694cbcb6131a3
vijaygupta23 <laxvijaygupta001@gmail.com>
2024-05-26 23:03:31 +0530

    new

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -62,9 +62,7 @@ function Freebook() {
         <div>
           <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
-            corporis nulla non suscipit, iure neque earum?
+            Here presented some free category books.
           </p>
         </div>
 
